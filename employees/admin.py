4beaae18ceeee4ba9ce3346065686322ab5f2ed1
bruno melo <brunousml@gmail.com>
2012-12-31 17:27:41 -0200

    tabelas relacionadas

--- employees/admin.py
+++ employees/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from employees.models import Employees
+from django.contrib.auth.models import User
+from employees.models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'last_login')
+    search_fields = ['username', 'email']
+    admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Employees)
+admin.site.register(Department)
+admin.site.register(Position)
--- employees/models.py
+++ employees/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Employees(models.Model):
     name = models.CharField(max_length=200)
-    pic = models.ImageField(upload_to='uploads/%Y/%m/%d/%H/%M/%S/')
+    pic = models.ImageField(upload_to='uploads/%Y/%m/%d/')
     email = models.EmailField()
     status = models.BooleanField()
     phone = models.CharField(max_length=200)
     birth = models.DateField()
     create_at = models.DateTimeField('date published')
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "%s" % (self.name)
 
 class Department(models.Model):
-    employee = models.ForeignKey(Employees)
+    employees = models.ManyToManyField(Employees)
+    name = models.CharField(max_length=200)
     create_at = models.DateTimeField('date published')
 
+    def __unicode__(self):
+        return "%s" % (self.name)
+
 class Position(models.Model):
-    employee = models.ForeignKey(Employees)
+    department = models.ManyToManyField(Department)
+    name = models.CharField(max_length=200)
     create_at = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+    
